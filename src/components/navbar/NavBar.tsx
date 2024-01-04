085778b8a85f8c074efb16ad37635b7709e97257
dimitris23bp <dimitris23bp@protonmail.com>
2024-01-04 19:46:41 +0200

    Update logo import path

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -9,7 +9,7 @@ import {
     Hidden,
     Box,
 } from "@mui/material";
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import { Link } from "react-router-dom";
