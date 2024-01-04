a1900a5d11a2ad4b29b39fb27584c1a2aa95bc1b
dimitris23bp <dimitris23bp@protonmail.com>
2024-01-05 00:14:01 +0200

    Update Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,52 @@
-import { Box } from "@mui/material";
+import { Box, IconButton, Link } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
     return (
         <Box
             sx={{
-                bgcolor: "orange",
+                bgcolor: "primary.main",
                 height: "100px",
                 bottom: 0,
                 width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "20px",
             }}
         >
-            Footer
+            <Link
+                href='https://instagram.com/dimakaramanis'
+                target='_blank'
+                color='inherit'
+            >
+                <IconButton>
+                    <InstagramIcon />
+                </IconButton>
+                @dimaKaramanis
+            </Link>
+            <Link
+                href='https://linkedin.com/in/dimitris23bp'
+                target='_blank'
+                color='inherit'
+            >
+                <IconButton>
+                    <LinkedInIcon />
+                </IconButton>
+                /in/dimitris23bp
+            </Link>
+            <Link
+                href='https://github.com/dimitris23bp'
+                target='_blank'
+                color='inherit'
+            >
+                <IconButton>
+                    <GitHubIcon />
+                </IconButton>
+                /dimitris23bp
+            </Link>
         </Box>
     );
 };
